2321034fbd602b7d4827ae7116c1479be205cbe4
shivam54245 <shivam54245@gmail.com>
2026-02-01 13:25:32 +0530

    Add initial configuration files for C/C++ development in VSCode

---
+++ Program 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Shivam Says: \"Hello Everynyan! I wish I was a bird\".\n Girl replies: \"OHH MYY GODDDDD!!!\"\n");
+    printf("\n\n\n\n\nNarrator:\"Basho\"");
+
+}
