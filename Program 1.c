641cb27ffd97176223a1e3cf0cebe19bc4f2cf83
shivam54245 <shivam54245@gmail.com>
2026-02-03 01:36:12 +0530

    wrote a sum program finally

--- Program 1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Shivam Says: \"Hello Everynyan! I wish I was a bird\".\n Girl replies: \"OHH MYY GODDDDD!!!\"\n");
-    printf("\n\n\n\n\nNarrator:\"Basho\"");
-
-}
---
+++ Program1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    while (1) {
+
+        int int1, int2;
+        printf("Enter two integers: \n");
+        scanf("%d %d", &int1, &int2);
+        int sum = int1 + int2;
+        printf("The sum of %d and %d is %d\n\n\n\n\n", int1, int2, sum);
+    }
+
+}
