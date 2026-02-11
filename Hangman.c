227f5de286f2ef7ad56d955e747ea72b0727ddea
shivam54245 <shivam54245@gmail.com>
2026-02-11 08:42:43 +0530

    new project begins

---
+++ Hangman.c
@@ -0,0 +1,36 @@
+/*
+
+TODO:
+
+    ESSENTIAL:
+        word list to choose from
+        word input processing
+        word comparing
+        word showing if correct
+        repeat 7?? times
+
+    OPTIONAL:
+        different modes (words, movies, shows, countries, etc)
+        read from different text files for each
+        wordle type stuff, like if a letter is present but not in the correct spot
+        hangman drawing, maybe a actual drawing or maybe text art or something idk
+
+*/
+
+
+// test word
+char target_word[100] = "Hundreds of beavers";
+int attempts_count = 0;
+
+int main() {
+
+    // code for printing stuff output ig
+   
+    // letter   or guess the answer
+    char attempt_mode[2];
+    attempts_count += 1
+    
+
+}
+
+
