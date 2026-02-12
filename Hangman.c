eb311376ad365576b412d061c5a7a5373e510878
shivam54245 <shivam54245@gmail.com>
2026-02-12 13:00:06 +0530

    hua thora hua

--- Hangman.c
+++ Hangman.c
@@ -19,26 +19,31 @@ TODO:
 
 
 // test word
-char target_word[] = "Hundreds of beavers";
+
+#include <stdio.h>
+char target_word[] = "hundred";
 int attempts_count = 0;
 
 // Output module
 void Output_text(char show_these_letters[]) {
+
     for (int letter_index_for_target_word = 0; target_word[letter_index_for_target_word] != '\0'; letter_index_for_target_word++) {
         
         // check if letter exists
-        for (int letter_index_for_show_these_letters = 0; show_these_letters[letter_index_for_show_these_letters] != '\0'; letter_index_for_show_these_letters++) {
+        for (int letter_index_for_show_these_letters = 0; show_these_letters[letter_index_for_show_these_letters  + 1] != '\0'; letter_index_for_show_these_letters++) {
             if (target_word[letter_index_for_target_word] == show_these_letters[letter_index_for_show_these_letters]) {
-                printf('%c', target_word[letter_index_for_target_word]);
+                printf("%c", target_word[letter_index_for_target_word]);
             }
 
             else {
-                    printf(" | __ ");
+                    printf("| __ ");
             }
         
-        printf(" | \n");
         }
     }
+
+printf("|");
+
 }
 
 
@@ -49,9 +54,10 @@ int main() {
 char letters_to_show[] = "";
    while (1) {
     char guessed_letter;
-    scanf("Enter the letter you want to use: " , guessed_letter);
-    char letters_to_show = letters_to_show + guessed_letter;
-    Output_text(letters_to_show);
+    printf("\n\n\nEnter the letter you want to use: ");
+    scanf(" %c", &guessed_letter);
+    char letters_to_show = letters_to_show + guessed_letter;         /// PROBLEM
+    Output_text(&letters_to_show);
 
    }
     // letter attempt or guess the answer
